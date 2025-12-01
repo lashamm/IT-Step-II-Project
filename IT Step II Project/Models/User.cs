42064b9b9e0887fe49902a02de21a55d0cdb1571
lasha <maisuradze.llasha@gmail.com>
2025-12-01 23:22:58 +0400

    final (hopefully)

--- IT Step II Project/Models/User.cs
+++ IT Step II Project/Models/User.cs
@@ -27,7 +27,7 @@ namespace IT_Step_II_Project.Models
         //////
 
 
-        //// პოლიმორფიზმი
+        //// პოლიმორფიზმი overloading
         /// <summary>
         /// კონსტრუქტორი User class-ისთვის
         /// </summary>
